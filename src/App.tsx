da4e67aa405e7631c04796fd4a9e5c116cc1bbc4
datnguyex <nnguyendat72@gmail.com>
2025-02-05 14:12:28 +0700

    create elearning project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+
+export default function App() {
+    return (
+        <Router>
+            <Routes></Routes>
+        </Router>
+    );
+}
