76a5bbb3d8e522400338853e29f8e773061c8558
datnguyex <nnguyendat72@gmail.com>
2025-02-19 15:56:18 +0700

    fixed path to firebase version3

--- src/services/SubjectService/FetchSubjectsService.ts
+++ src/services/SubjectService/FetchSubjectsService.ts
@@ -1,24 +1,22 @@
 // services/FetchSubjectsService.ts
-import { getDatabase, ref, get } from 'firebase/database';
-import { app } from './../../firebase';
+import { ref, get } from 'firebase/database';
+import { Databasez } from './../../firebase';
 import FetchSubjectsServiceProps from '~/types/FetchSubjectServiceProps/FetchSubjectServiceProps';
 
-const FetchSubjects = ({ setSubjects }: FetchSubjectsServiceProps) => {
-    const database = getDatabase(app);
-    const subjectsRef = ref(database, 'Elearning/Semester/Semester1/Schedule/Subject');
-    get(subjectsRef)
-        .then((snapshot) => {
-            if (snapshot.exists()) {
-                const data = snapshot.val();
-                const subjectsArray = Object.values(data);
-                setSubjects(subjectsArray);
-            } else {
-                console.log('No data available');
-            }
-        })
-        .catch((error) => {
-            console.error('Error loading data from Firebase:', error);
-        });
+const FetchSubjects = async ({ setSubjects }: FetchSubjectsServiceProps) => {
+    try {
+        const subjectsRef = ref(Databasez, 'Elearning/Semester/Semester1/Schedule/Subject');
+        const snapshot = await get(subjectsRef);
+        if (snapshot.exists()) {
+            const data = snapshot.val();
+            const subjectsArray = Object.values(data);
+            setSubjects(subjectsArray);
+        } else {
+            console.log('No data available');
+        }
+    } catch (error) {
+        console.error('Error loading data from Firebase:', error);
+    }
 };
 
 export default FetchSubjects;
