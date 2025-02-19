047c2275633c3c893739c06234a5f5697f5bb75f
datnguyex <nnguyendat72@gmail.com>
2025-02-19 16:21:54 +0700

    fixed path to firebase version6

--- src/services/SubjectService/FetchSubjectsService.ts
+++ src/services/SubjectService/FetchSubjectsService.ts
@@ -1,23 +1,23 @@
-import { ref, get } from 'firebase/database';
-import { Databasez } from './../../firebase';
-// hoặc các dịch vụ Firebase bạn sử dụng
+// import { ref, get } from 'firebase/database';
+// import { Databasez } from './../../firebase';
+// // hoặc các dịch vụ Firebase bạn sử dụng
 
 import FetchSubjectsServiceProps from '~/types/FetchSubjectServiceProps/FetchSubjectServiceProps';
 
 const FetchSubjects = async ({ setSubjects }: FetchSubjectsServiceProps) => {
-    try {
-        const subjectsRef = ref(Databasez, 'Elearning/Semester/Semester1/Schedule/Subject');
-        const snapshot = await get(subjectsRef);
-        if (snapshot.exists()) {
-            const data = snapshot.val();
-            const subjectsArray = Object.values(data);
-            setSubjects(subjectsArray);
-        } else {
-            console.log('No data available');
-        }
-    } catch (error) {
-        console.error('Error loading data from Firebase:', error);
-    }
+    // try {
+    //     const subjectsRef = ref(Databasez, 'Elearning/Semester/Semester1/Schedule/Subject');
+    //     const snapshot = await get(subjectsRef);
+    //     if (snapshot.exists()) {
+    //         const data = snapshot.val();
+    //         const subjectsArray = Object.values(data);
+    //         setSubjects(subjectsArray);
+    //     } else {
+    //         console.log('No data available');
+    //     }
+    // } catch (error) {
+    //     console.error('Error loading data from Firebase:', error);
+    // }
 };
 
 export default FetchSubjects;
