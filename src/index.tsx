c9b275dcc306363926eb44de4539a47760431c2e
datnguyex <nnguyendat72@gmail.com>
2025-02-25 18:32:59 +0700

    update something

--- src/index.tsx
+++ src/index.tsx
@@ -8,14 +8,13 @@ import { Provider } from 'react-redux';
 import { StoreReducer } from './redux/Store';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-    // <React.StrictMode>
-
-    // </React.StrictMode>
-    <Provider store={StoreReducer}>
-        <GlobalStyles>
-            <App />
-        </GlobalStyles>
-    </Provider>,
+    <React.StrictMode>
+        <Provider store={StoreReducer}>
+            <GlobalStyles>
+                <App />
+            </GlobalStyles>
+        </Provider>
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
